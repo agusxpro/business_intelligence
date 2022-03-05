f0c1307c770111cef1e4f5f1cebd0450b953a54b
agusxpro <92310438+agusxpro@users.noreply.github.com>
2022-03-05 16:00:56 +0700

    Add files via upload

---
+++ assignment02.py
@@ -0,0 +1,79 @@
+import mysql.connector as mysql
+from mysql.connector import Error
+import sqlalchemy
+
+class AutoCreate:
+
+    def _init_(self, host, port, user, password):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+
+    def create_db(self, db_name):
+        conn = mysql.connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            passwd=self.password
+        )
+        try:
+            if conn.is_connected():
+                cursor = conn.cursor()
+                cursor.execute("CREATE DATABASE {}".format(db_name))
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+        # preparing a cursor object
+        # creating database
+
+    def create_table(self, db_name, table_name, df):
+        conn = mysql.connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            passwd=self.password
+        )
+        try:
+            if conn.is_connected():
+                cursor = conn.cursor()
+                cursor.execute("USE {}".format(db_name))
+                cursor.execute("CREATE TABLE {}".format(table_name))
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+
+        engine_stmt = 'mysql+mysqldb://%s:%s@%s:%s/%s' % (self.user, urlquote(self.password),
+                                                            self.host, self.port, db_name)
+        engine = sqlalchemy.create_engine(engine_stmt)
+
+        df.to_sql(name=table_name, con=engine,
+                  if_exists='append', index=False, chunksize=1000)
+
+    def load_data(self, db_name, table_name):
+        conn = mysql.connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            passwd=self.password
+        )
+        try:
+            if conn.is_connected():
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM {}.{}".format(db_name, table_name))
+                result = cursor.fetchall()
+                return result
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+
+    def ambil_csv(self, path):
+        return pd.read_csv(path, index_col=False, delimiter=',')
+
+    print("This Function has been established!")
+
+test = AutoCreate()
+test.host = "localhost"
+test.port = "3306"
+test.user = "root"
+test.password="MyNewPass"
+
+#test.create_db("AutoCreate_db1")
+test.create_table("AutoCreate_db1", "tbl_address", "df")
